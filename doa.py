0345131c65ea1d029db0fb683ae48c1de0987c6b
smagnuszewski <szymoon00m@gmail.com>
2025-02-26 18:00:20 +0000

    WiP: read sst data to json format

--- doa.py
+++ doa.py
@@ -1,8 +1,21 @@
 import subprocess
-# import json
+import json
+
 odas_process = subprocess.Popen(["/home/rpi/odas/build/bin/odaslive", "-c", "./custom.cfg"],stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+parsed_data = []
+counter = 0
+
 while True:
     line = odas_process.stdout.readline()
+    line = line.strip()
     if not line:
-        break
+        continue
     print(line)
+    if line.startswith("{"):
+        l = line.removesuffix(",")
+        # print(line)
+        try:
+            data = json.loads(l)
+            # print(data)
+        except json.JSONDecodeError as e:
+            print(f"Error parsing JSON: {e}")
