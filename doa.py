6ec8ad8e48355ec4b90285b39691e3457a5813eb
smagnuszewski <szymoon00m@gmail.com>
2025-03-03 10:12:37 +0000

    Handle single sound source

--- doa.py
+++ doa.py
@@ -1,15 +1,12 @@
 import subprocess
 import json
 import math
+from leds import start_leds, stop_leds, light
+import time
+import sys
 
-odas_process = subprocess.Popen(["/home/rpi/odas/build/bin/odaslive", "-c", "./custom.cfg"],stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-parsed_data_x = []
-x_av=0
-parsed_data_y = []
-y_av=0
-parsed_data_activity = []
-ac_av=0
-counter = 0
+
+led=start_leds(5)
 
 
 def average(array:list[float])->float:
@@ -17,37 +14,56 @@ def average(array:list[float])->float:
     for i in array:
         av+=i
     return av/len(array)
-        
 
-while True:
-    line = odas_process.stdout.readline()
-    line = line.strip()
-    if not line:
-        continue
-    # print(line)
-    if line.startswith("{"):
-        l = line.removesuffix(",")
+        
+def main():
+    odas_process = subprocess.Popen(["/home/rpi/odas/build/bin/odaslive", "-c", "./custom.cfg"],stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    parsed_data_x = []
+    x_av=0
+    parsed_data_y = []
+    y_av=0
+    parsed_data_activity = []
+    ac_av=0
+    counter = 0
+
+    while True:
+        line = odas_process.stdout.readline()
+        line = line.strip()
+        if not line:
+            continue
         # print(line)
-        try:
-            data = json.loads(l)
-            if data['id']!=0:
-                parsed_data_x.append(data['x'])
-                parsed_data_y.append(data['y'])
-                parsed_data_activity.append(data['activity'])
-                counter = counter + 1        
-            # print(data)
-        except json.JSONDecodeError as e:
-            pass
-            # print(f"Error parsing JSON: {e}")
-    if counter>=99:
-        x_av=average(parsed_data_x)
-        y_av=average(parsed_data_y)
-        ac_av=average(parsed_data_activity)
-        angle = math.atan(x_av/y_av)
-        print(f"x={x_av}, y={y_av}, activity={ac_av}, angle={angle}")
-        counter = 0
-        parsed_data_activity.clear()
-        parsed_data_x.clear()
-        parsed_data_y.clear()
+        if line.startswith("{"):
+            l = line.removesuffix(",")
+            # print(line)
+            try:
+                data = json.loads(l)
+                if data['id']!=0:
+                    parsed_data_x.append(data['x'])
+                    parsed_data_y.append(data['y'])
+                    parsed_data_activity.append(data['activity'])
+                    counter = counter + 1        
+                # print(data)
+            except json.JSONDecodeError as e:
+                pass
+                # print(f"Error parsing JSON: {e}")
+        if counter>=99:
+            x_av=average(parsed_data_x)
+            y_av=average(parsed_data_y)
+            ac_av=average(parsed_data_activity)
+            angle = math.degrees(math.atan2(y_av,x_av))+180
+            print(f"x={x_av}, y={y_av}, activity={ac_av}, angle={angle}")
+            light(strip=led,angle=angle)
+            counter = 0
+            parsed_data_activity.clear()
+            parsed_data_x.clear()
+            parsed_data_y.clear()
         
         
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        stop_leds(led,5)
+        print ('interrupted, turning off')
+        time.sleep(1)
+        sys.exit(0)
--- leds.py
+++ leds.py
@@ -4,6 +4,25 @@ from apa102_pi.driver import apa102
 import time
 import RPi.GPIO  as GPIO
 
+def start_leds(power_pin):
+    power_pin = 5
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(power_pin,GPIO.OUT)
+    GPIO.output(power_pin,GPIO.HIGH)
+    strip = apa102.APA102(num_led=12)
+    return strip
+
+def light(strip,angle):
+    strip.clear_strip()
+    led_nr=(round((angle/30)))
+    strip.set_pixel_rgb(led_nr, 0x00FF00)  # Green
+    strip.show()
+
+def stop_leds(strip,power_pin):
+    strip.clear_strip()
+    strip.cleanup()
+    GPIO.output(power_pin,GPIO.LOW)
+
 
 def main():
     # Initialize power pin
